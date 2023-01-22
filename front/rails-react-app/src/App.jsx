9605eb54d45ade01ad57115f5f12a2a11163c4fc
hanamuratukito <keikennouta11@gmail.com>
2023-01-22 12:55:54 +0900

    add：フロント：コメント追加

--- front/rails-react-app/src/App.jsx
+++ front/rails-react-app/src/App.jsx
@@ -5,12 +5,18 @@ import { IncompleteTodos } from "./components/IncompleteTodos";
 import { CompleteTodos } from "./components/CompleteTodos";
 
 export const App = () => {
+  // TODO入力を管理する変数　
   const [todoText, setTodoText] = useState("");
+
+  // 未完了のTODOを管理する変数
   const [incompleteTodos, setIncompleteTodos] = useState([]);
+
+  // 完了のTODOを管理する変数
   const [completeTodos, setCompleteTodos] = useState([]);
 
   const onChangeTodoText = (event) => setTodoText(event.target.value);
 
+  // 追加ボタン押下時の処理
   const onClickAdd = () => {
     if (todoText === "") return;
     const newTodos = [...incompleteTodos, todoText];
@@ -18,28 +24,35 @@ export const App = () => {
     setTodoText("");
   };
 
+  // 削除ボタン押下時の処理
   const onClickDelete = (index) => {
     const newTodos = [...incompleteTodos];
     newTodos.splice(index, 1);
     setIncompleteTodos(newTodos);
   };
 
+  // 完了ボタン押下時の処理
   const onClickComplete = (index) => {
+    // 未完了項目を更新
     const newIncompleteTodos = [...incompleteTodos];
     newIncompleteTodos.splice(index, 1);
-
-    const newCompleteTodos = [...completeTodos, incompleteTodos[index]];
     setIncompleteTodos(newIncompleteTodos);
+
+    // 完了項目を更新
+    const newCompleteTodos = [...completeTodos, incompleteTodos[index]];
     setCompleteTodos(newCompleteTodos);
   };
 
+  // 戻るボタン押下時の処理
   const onClickback = (index) => {
+    // 完了項目を更新
     const newCompleteTodos = [...completeTodos];
     newCompleteTodos.splice(index, 1);
+    setCompleteTodos(newCompleteTodos);
 
+    // 未完了項目を更新
     const newIncompleteTodos = [...incompleteTodos, completeTodos[index]];
     setIncompleteTodos(newIncompleteTodos);
-    setCompleteTodos(newCompleteTodos);
   };
 
   return (
