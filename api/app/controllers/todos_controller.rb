89d5755208b7bcb2937a8f9aecfe49178bd50f23
hanamuratukito <keikennouta11@gmail.com>
2023-01-22 16:46:02 +0900

    add：バックエンドのメソッド追加
    現状は使用しないためコメントアウト

--- api/app/controllers/todos_controller.rb
+++ api/app/controllers/todos_controller.rb
@@ -1,5 +1,23 @@
 class TodosController < ApplicationController
   def getTodo
-    render json: { todo: Todo.all }
+    render json: { incompleteTodos: Todo.where(isDeleted: false, status: 1), completeTodos: Todo.where(isDeleted: false, status: 2) }
   end
+  # def addTodo(text)
+  #   todo = Todo.new
+  #   todo.status = 1 # 未完了
+  #   todo.text = text
+  #   todo.isDeleted = 0
+  #   todo.save
+  #   render json: { incompleteTodos: Todo.where(isDeleted: false, status: 1), completeTodos: Todo.where(isDeleted: false, status: 2) }
+  # end
+  # def updateTodo(id, status)
+  #   todo = todo.find(id)
+  #   todo.update(status: status)
+  #   render json: { incompleteTodos: Todo.where(isDeleted: false, status: 1), completeTodos: Todo.where(isDeleted: false, status: 2) }
+  # end
+  # def deleteTodo(id)
+  #   todo = todo.find(id)
+  #   todo.update(isDeleted: true)
+  #   render json: { incompleteTodos: Todo.where(isDeleted: false, status: 1), completeTodos: Todo.where(isDeleted: false, status: 2) }
+  # end
 end
--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'todos/getTodo'
+  # post 'todos/addTodo'
+  # post 'todos/updateTodo'
+  # post 'todos/deleteTodo'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
