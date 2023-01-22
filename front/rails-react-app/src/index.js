cf7ed9a75ec5df6a0273f7b15bdfccaccb4d5f01
hanamuratukito <keikennouta11@gmail.com>
2023-01-22 14:54:21 +0900

    fix：react18以降ではReactDom.renderは警告が出るため解消

--- front/rails-react-app/src/index.js
+++ front/rails-react-app/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
-import ReactDom from "react-dom";
+import ReactDom from "react-dom/client";
 import { App } from "./App";
 
-ReactDom.render(<App />, document.getElementById("root"));
+const root = ReactDom.createRoot(document.getElementById('root'));
+root.render(<App />)
