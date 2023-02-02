b0723be1b0804c4a84e5a70ca92058f8fc01e2e2
hanamuratukito <keikennouta11@gmail.com>
2023-02-02 21:48:17 +0900

    add：eslint追加

--- front/rails-react-app/src/App.jsx
+++ front/rails-react-app/src/App.jsx
@@ -1,23 +1,23 @@
-import axios from "axios";
-import React, { useState, useEffect } from "react";
-import "./styles.css";
-import { InputTodo } from "./components/InputTodo";
-import { IncompleteTodos } from "./components/IncompleteTodos";
-import { CompleteTodos } from "./components/CompleteTodos";
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import './styles.css';
+import { InputTodo } from './components/InputTodo';
+import { IncompleteTodos } from './components/IncompleteTodos';
+import { CompleteTodos } from './components/CompleteTodos';
 
 export const App = () => {
-  useEffect(()=>{
+  useEffect(() => {
     (async () => {
       const res = await axios.get('http://localhost:3001/todos/getTodo');
       setIncompleteTodos(res.data.incompleteTodos);
       setCompleteTodos(res.data.completeTodos);
-    
-      console.log(res);
-    })()
-  },[])
 
-  // TODO入力を管理する変数　
-  const [todoText, setTodoText] = useState("");
+      console.log(res);
+    })();
+  }, []);
+
+  // TODO入力を管理する変数
+  const [todoText, setTodoText] = useState('');
 
   // 未完了のTODOを管理する変数
   const [incompleteTodos, setIncompleteTodos] = useState([]);
@@ -29,30 +29,40 @@ export const App = () => {
 
   // 追加ボタン押下時の処理
   const onClickAdd = async () => {
-    if (todoText === "") return;
-    const res = await axios.post('http://localhost:3001/todos/addTodo', { text: todoText });
+    if (todoText === '') return;
+    const res = await axios.post('http://localhost:3001/todos/addTodo', {
+      text: todoText,
+    });
     setIncompleteTodos(res.data.incompleteTodos);
 
-    setTodoText("");
+    setTodoText('');
     console.log(res);
   };
 
   // 削除ボタン押下時の処理
   const onClickDelete = async (id) => {
-    const res = await axios.post('http://localhost:3001/todos/deleteTodo', { id: id });
+    const res = await axios.post('http://localhost:3001/todos/deleteTodo', {
+      id: id,
+    });
     setIncompleteTodos(res.data.incompleteTodos);
   };
 
   // 完了ボタン押下時の処理
   const onClickComplete = async (id) => {
-    const res = await axios.post('http://localhost:3001/todos/updateTodo', { id: id, status: 2 });
+    const res = await axios.post('http://localhost:3001/todos/updateTodo', {
+      id: id,
+      status: 2,
+    });
     setIncompleteTodos(res.data.incompleteTodos);
     setCompleteTodos(res.data.completeTodos);
   };
 
   // 戻るボタン押下時の処理
   const onClickback = async (id) => {
-    const res = await axios.post('http://localhost:3001/todos/updateTodo', { id: id, status: 1 });
+    const res = await axios.post('http://localhost:3001/todos/updateTodo', {
+      id: id,
+      status: 1,
+    });
     setIncompleteTodos(res.data.incompleteTodos);
     setCompleteTodos(res.data.completeTodos);
   };
--- front/rails-react-app/src/components/InputTodo.jsx
+++ front/rails-react-app/src/components/InputTodo.jsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react';
 
 const style = {
-  backgroundColor: "#c1ffff",
-  width: "400px",
-  height: "30px",
-  padding: "8px",
-  margin: "8px"
+  backgroundColor: '#c1ffff',
+  width: '400px',
+  height: '30px',
+  padding: '8px',
+  margin: '8px',
 };
 
 export const InputTodo = (props) => {
